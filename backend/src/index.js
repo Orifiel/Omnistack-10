03da10303de07ec72e8549ef146ff231d4121806
Orifiel <cleitonmattos87@gmail.com>
2023-12-11 10:30:39 -0300

    correção das dependências back/front

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,27 @@
 /**rotas são endereços adicionais do endereço principal
  * ex. www.omnistack.com
- *     www,omnistack.com/users
+ *     www.omnistack.com/users
  *
  */
-const express = require('express') //importando o express para a variavel express
-const mongoose = require('mongoose');
-const routes = require('./routes')
-const cors = require('cors')
-const app = express()
+const express = require("express"); //importando o express para a variavel express
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const cors = require("cors");
+const app = express();
 
-mongoose.connect('mongodb+srv://cleiton:qwerty123@cluster0-7psae.mongodb.net/week10?retryWrites=true&w=majority', 
-{
-useNewUrlParser: true,
-useUnifiedTopology: true
-})
+mongoose.connect(
+  "mongodb+srv://cleitonm:qwerty123@cluster0.jaisdtn.mongodb.net/",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-app.use(cors())
-app.use(express.json()) //deve vir antes das rotas
-app.use(routes)
+app.use(cors());
+app.use(express.json()); //deve vir antes das rotas
+app.use(routes);
 
-app.listen(3333)
+app.listen(3333);
 
 /** Metodos HTTP
  * GET, - listar, buscar informações
@@ -28,11 +30,11 @@ app.listen(3333)
  * DELETE - quero excluir uma informação
  */
 
- /** Tipos de parametros
-  * query: request.query (filtros, ordenação, paginação) usados no GET
-  * route: request.params (identificar um recurso na alteração ou remoção)
-  * body: request.body (dados para criação ou alteração de um registro)
-  */
+/** Tipos de parametros
+ * query: request.query (filtros, ordenação, paginação) usados no GET
+ * route: request.params (identificar um recurso na alteração ou remoção)
+ * body: request.body (dados para criação ou alteração de um registro)
+ */
 
 //MongoDB (DB não relacional)
 
